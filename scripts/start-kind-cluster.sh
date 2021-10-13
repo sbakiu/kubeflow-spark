ef6dc462e55c71456d035e20f9054c621bdd09ff
Sadik Bakiu <sbakiu@gmail.com>
2021-10-13 05:59:05 +0200

    Bump kubeflow version

--- scripts/start-kind-cluster.sh
+++ scripts/start-kind-cluster.sh
@@ -2,4 +2,4 @@
 #minikube start  --memory 8192 --cpus 4 --insecure-registry "10.0.0.0/24" --driver=hyperkit
 #minikube addons enable metrics-server
 #minikube addons enable ingress
-kind create cluster --name spark-kf --wait 30s
+kind create cluster --wait 30s --name spark-kf
