cc436777c0e140435ceffa345020a574614cd50e
Sadik Bakiu <sbakiu@gmail.com>
2021-11-08 09:33:34 +0100

    Fix k8s version for minikube start

--- scripts/start-minikube.sh
+++ scripts/start-minikube.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-minikube start  --memory 8192 --cpus 4 --insecure-registry "10.0.0.0/24" --driver=hyperkit
+minikube start  --memory 8192 --cpus 4 --insecure-registry "10.0.0.0/24" --driver=hyperkit --kubernetes-version=v1.20.0
 minikube addons enable metrics-server
 minikube addons enable ingress
