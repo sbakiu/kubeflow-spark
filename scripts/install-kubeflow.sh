184dc09b28ef59f1539d8983c4b2271e9057a3b8
Sadik Bakiu <sbakiu@gmail.com>
2021-12-02 11:42:52 +0100

    Add short sleep

--- scripts/install-kubeflow.sh
+++ scripts/install-kubeflow.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # From https://www.kubeflow.org/docs/components/pipelines/installation/localcluster-deployment/#deploying-kubeflow-pipelines
 export PIPELINE_VERSION=1.7.1
+sleep 5
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
 kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/env/platform-agnostic-pns?ref=$PIPELINE_VERSION" && \
-kubectl rollout status  -n kubeflow deployment/ml-pipeline -w
+kubectl rollout status -n kubeflow deployment/ml-pipeline -w
 
 # kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/env/dev?ref=$PIPELINE_VERSION"
 # kubectl delete -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
