98a51a2702f6350dd693ae27100787f2ca755f00
Sadik Bakiu <sbakiu@gmail.com>
2022-05-26 10:36:51 +0200

    Upgrade kubeflow pipelines version to 1.8.1 Use k8s versions 1.22.9 Use docker drivers for minikube

--- scripts/install-kubeflow.sh
+++ scripts/install-kubeflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # From https://www.kubeflow.org/docs/components/pipelines/installation/localcluster-deployment/#deploying-kubeflow-pipelines
-export PIPELINE_VERSION=1.7.1
+export PIPELINE_VERSION=1.8.1
 sleep 5
 kubectl apply -k "github.com/kubeflow/pipelines/manifests/kustomize/cluster-scoped-resources?ref=$PIPELINE_VERSION"
 kubectl wait --for condition=established --timeout=60s crd/applications.app.k8s.io
